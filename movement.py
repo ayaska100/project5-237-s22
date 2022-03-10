7fc614013700d840ea0e17559f5ef2fc28667e05
yushia askarpour <ayaska@pm.me>
2022-03-10 10:16:40 -0500

    redo move_cm to use rotations instead of time

--- movement.py
+++ movement.py
@@ -1,37 +1,68 @@
+#!/usr/bin/python3
 from ev3dev2.motor import MoveTank
 
-# extend MoveTank class into custom class
+
 class tank237(MoveTank):
-    def move_cm(self, distance):  # move a certain amount of cm
+    # extend MoveTank class into custom class
+    # prefix new functions with c_ for custom
+
+    def c_move_cm_time(self, distance):
+        '''Depricated (use c_move_cm instead).
+        Move a certain distance (cm) forward (positive) or backwards (negative).'''
         # this impacts the distance - calibrate the time around it
-        abs_speed = 40
+        speed = 30
 
         # the motors can't move for a negative time, but they
         # can move for a positive time at a negative speed
-        if (distance > 0):  # if positive
-            speed = abs_speed
-        elif (distance < 0):  # if negative
-            speed = -abs_speed
+        if (distance < 0):  # if negative
+            speed *= -1
 
         # this coefficient was taken through emperical testing
         # NOTE!!!!! this is also dependent on the motor speed!
         # it must be changed if a new speed is chosen
-        cm_per_second = 17.2
+        seconds_per_cm = 0.063
 
         # take the absolute value because we cannot have negative time
-        time = abs(distance / cm_per_second)
+        # extra bit because. robot undershot short distances but overshot large distances
+        time = seconds_per_cm * abs(distance) + 0.0075
 
         # on_for_seconds arguments:
         # left speed (0-100), right speed (0-100), time (s)
         self.on_for_seconds(speed, speed, time)
 
-    def turn_180_manual(self):  # manual function
+    # wheel diameter 5.6cm = about 17.6 cm circumference - tune to reality
+    def c_move_cm(self, distance):
+        # this function is speed indepenendent bc it track the rotation of the
+        # wheel which is inherently tied to distance traveled.
+        speed = 35
+
+        if (distance < 0):  # if negative
+            speed *= -1
+
+        rotations = distance / 17.565
+        self.on_for_rotations(speed, speed, rotations)
+
+    def c_turn_degrees(self, degrees):
+        '''Manual (hand-tuned) function:
+        turns x degrees to the right (negative for left).'''
+        # problem: this function is inherently a bit inaccurate because
+        # the servos resist movement but have a small wiggle room
+        # TODO: use smaller wheels?
+
         # from experience it's easier to calibrate turning on a lower speed
         # plus it leads to a more consistent turn because less jerk
         speed = 25
-        time = 1.32
-        self.on_for_seconds(speed, -speed, time)  # turns clockwise
 
-    def turn_gyro(self, degrees): # turns x degrees cw
-        self.turn_degrees(speed=30, target_angle=degrees)
+        # reverse speed if negative degrees (left turn)
+        if degrees < 0:
+            speed *= -1
 
+        # constant for converting degrees of turn to seconds of motor runtime
+        # units: seconds per full rotation
+        coefficient = 2.69  # edit this line
+
+        # abs bc time cannot be negative
+        time = coefficient * abs(degrees / 360)
+
+        # aligned to speed. if speed is positive then robot turns clockwise.
+        self.on_for_seconds(speed, -speed, time)
--- test_motors.py
+++ test_motors.py
@@ -1,22 +1,12 @@
+#!/usr/bin/python3
 from movement import tank237
 from time import sleep
 from ev3dev2.sensor.lego import GyroSensor
-from ev3dev2 import SpeedPercent
+# from ev3dev2 import SpeedPercent
 
 # initialization
-tank = tank237('outA', 'outD')
+tank = tank237('outA', 'outD')  # left/right motors
 
-# Initialize the tank's gyro sensor; calibrate gyro sensor and reset position to 0
-tank.gyro = GyroSensor()
-tank.gyro.calibrate()
-tank.gyro.reset()
-
-# Pivot 30 degrees
-tank.turn_right(
-    speed=SpeedPercent(5),
-    degrees=30
-)
-
-# movement.turn_180_manual(tank)
-
-# movement.move_cm(50)
+tank.c_move_cm(2.54 * 12)
+tank.c_turn_degrees(-90)
+tank.c_move_cm(2.54 * 84)
